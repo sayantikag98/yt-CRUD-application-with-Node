3052a80000c0653cd968bd9de01622ba63620f1d
Sayantika Ghosh <sayantikaghosh98@gmail.com>
2021-10-22 14:41:58 +0530

    Added controllers folder

---
+++ controllers/users.js
@@ -0,0 +1,41 @@
+import {v4 as uuidv4} from "uuid";
+
+let users = [];
+
+export const getAllUsers = (req, res) => {
+    if(users.length === 0) res.send("User list is empty.");
+    else res.send(users);
+}
+
+export const addUser = (req, res) => {
+    users.push({...req.body, id:uuidv4()});  // adding a new key called id
+    res.send(`New user ${req.body.firstName} added to the list.`);
+}
+
+export const getParticularUser = (req, res) => {
+    const user = users.find(ele => ele.id === req.params.id);
+    if(!user) res.send(`No user with id ${req.params.id} exists in the list.`);
+    else res.send(user);
+}
+
+export const deleteUser = (req, res) => {
+    const user = users.find(ele => ele.id === req.params.id);
+    if(!user) res.send(`No user with id ${req.params.id} exists in the list.`);
+    else{
+        users = users.filter(ele=> ele !== user);
+        res.send(`User ${user.firstName} was deleted from the list.`);
+    }    
+}
+
+export const updateUser = (req, res) => {
+    const user = users.find(ele => ele.id === req.params.id);
+    if(!user) res.send(`No user with id ${req.params.id} exists in the list.`);
+    else{
+        const {firstName, lastName, age} = req.body;
+        if(firstName) user.firstName = firstName;
+        if(lastName) user.lastName = lastName;
+        if(age) user.age = age;
+        res.send(`User with id ${req.params.id} information updated.`);
+    }    
+}
+
--- routes/users.js
+++ routes/users.js
@@ -1,41 +1,22 @@
 import express from "express";
-import {v4 as uuidv4} from "uuid";
+import {getAllUsers, addUser, getParticularUser, deleteUser, updateUser} from "../controllers/users.js" // filename should have .js extension
 
 // all routes in here are starting with /users
 const router = express.Router();
-let users = [];
-
 
    
 // Getting all the user's info using the GET request
-router.get("/", (req, res) => {
-    if(users.length === 0) res.send("User list is empty.");
-    else res.send(users);
-});
+router.get("/", getAllUsers);
 
 // Posting an user info using the POST request
-router.post("/", (req, res) => {
-    users.push({...req.body, id:uuidv4()});  // adding a new key called id
-    res.send(`New user ${req.body.firstName} added to the list.`);
-});
+router.post("/", addUser);
 
 // Getting a particular user info through the unique id
 // :id means there we can put anything but only id would mean id in literal sense which we don't want
-router.get("/:id", (req, res) => {
-    const user = users.find(ele => ele.id === req.params.id);
-    if(!user) res.send(`No user with id ${req.params.id} exists in the list.`);
-    else res.send(user);
-});
+router.get("/:id", getParticularUser);
 
 // Deleting a particular user with the given id with the DELETE request
-router.delete("/:id", (req, res) => {
-    const user = users.find(ele => ele.id === req.params.id);
-    if(!user) res.send(`No user with id ${req.params.id} exists in the list.`);
-    else{
-        users = users.filter(ele=> ele !== user);
-        res.send(`User ${user.firstName} was deleted from the list.`);
-    }    
-});
+router.delete("/:id", deleteUser);
 
 // Updating a particular info of a user with the given id with the PATCH request
 // difference between PUT and PATCH request
@@ -43,19 +24,12 @@ router.delete("/:id", (req, res) => {
     PUT completely overwrites or changes everything
     PATCH updates certain info as provided and does not overwrite
 */
-router.patch("/:id", (req, res) => {
-    const user = users.find(ele => ele.id === req.params.id);
-    if(!user) res.send(`No user with id ${req.params.id} exists in the list.`);
-    else{
-        const index = users.indexOf(user);
-        const {firstName, lastName, age} = req.body;
-        if(firstName) user.firstName = firstName;
-        if(lastName) user.lastName = lastName;
-        if(age) user.age = age;
-        users[index] = user;
-        res.send(`User with id ${req.params.id} information updated.`);
-    }    
-});
+router.patch("/:id", updateUser);
 
 
 export default router;
+
+/*
+Whenever you are saving the file users.js then nodemon is restarting the server 
+then users array becomes an empty array
+*/
